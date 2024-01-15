dc6d5bc08096765078a990e7130bd9b0ea0b808b
TheActualSanny <148360536+TheActualSanny@users.noreply.github.com>
2024-01-16 02:23:49 +0400

    Update database-manager.py
    Fixed functionality

--- database-manager.py
+++ database-manager.py
@@ -1,28 +1,30 @@
 import psycopg2
 import os
 from dotenv import load_dotenv
-psycopg2.connect()
+from mainscraper import Laptop, laptop_list
+
+conn = psycopg2.connect(host = os.getenv('HOST'), database = os.getenv('DB'), user = os.getenv('NAME'), password = os.getenv('PASS'))
 cursor = conn.cursor()
+
 cursor.execute("""CREATE TABLE IF NOT EXISTS laptop_data(id integer PRIMARY KEY,
 model text,
 current_price real,
 processor test,
 os text,
-praphics text,
+graphics text,
 memory text,
 storage text,
 display text
 )""")
-import psycopg2
-from main_scraper import Laptop, laptop_list
 
-
+index = 1
 for laptop in laptop_list:
     with conn:
         cursor.execute('''
-            INSERT INTO laptops (model, curr_price, processor, os, graphics, memory, storage)
-            VALUES (%s, %s, %s, %s, %s, %s, %s)
+            INSERT INTO laptop_data(id, model, current_price, processor, os, graphics, memory, storage, display)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
         ''', (
+            index
             laptop.model,
             laptop.curr_price,
             laptop.processor,
@@ -30,7 +32,10 @@ for laptop in laptop_list:
             laptop.graphics,
             laptop.memory,
             laptop.storage
+            laptop.display
         ))
+    index += 1
+    
 conn.commit()
 conn.close()
 cursor.close()
